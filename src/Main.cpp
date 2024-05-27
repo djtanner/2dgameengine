0de620f519c9b214ddac6e8471d3fc0336ee26cf
Zhila Shariat <zshariat08@gmail.com>
2024-05-27 11:20:27 -0500

    game window and other game class functions

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_mixer.h>
-#include <glm/glm.hpp>
-#include <imgui/imgui.h>
-#include <imgui/imgui_sdl.h>
-#include <sol/sol.hpp>
+#include "Game.h"
 
-int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
-    std::cout << "Hello, world!" << std::endl;
+
+int main(int argc, char* argv[]) {
+  
+    Game game;
+
+    game.Initialize();
+    game.Run();
+    game.Destroy();
     return 0;
 }
