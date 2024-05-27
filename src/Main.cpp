d21b35357dcdaec282f9ba697df6e9cf03137783
Zhila Shariat <zshariat08@gmail.com>
2024-05-27 01:26:43 -0500

    updated make and teting dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <imgui/imgui_sdl.h>
+#include <sol/sol.hpp>
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
